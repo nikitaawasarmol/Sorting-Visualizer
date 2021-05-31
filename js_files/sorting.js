a8d9e17152aed036a172985e03bb96dc79cef4ae
nikitaawasarmol <nikitaawasarmol@gmail.com>
2021-05-31 22:01:08 +0530

    Added bars

--- js_files/sorting.js
+++ js_files/sorting.js
@@ -1,39 +1,32 @@
-// pushing number of bars
 let arraySize = document.querySelector('#arr_sz');
-arraySize.addEventListener('input', function(){
-    console.log(arraySize.value, typeof(arraySize.value));
-    createNewArray(parseInt(arraySize.value));
-});
-var arr = [];
+
+let array = [];
+
+
 createNewArray();
 
-function createNewArray() {
-while(arr.length < 100){
-    var r = Math.floor(Math.random() * 100) + 1;
-    if(arr.indexOf(r) === -1) arr.push(r);
+function createNewArray(noOfBars = 50) {
+   
+    array = [];
+    for (let i = 0; i < noOfBars; i++) {
+    array.push(Math.floor(Math.random() * 250) + 1);
 }
+console.log(array)
 
-const bars = document.getElementById('#bars');
+const bars = document.querySelector('#bars');
 
-for(let i = 0; i < 100; i++) {
+for(let i = 0; i < noOfBars; i++) {
 const bar = document.createElement("div");
-bar.style.height = `${arr[i] * 2}px`
-bar.classList.add('bar');
-bar.classList.add(`barNo${i}`);
+ bar.style.height = `${array[i] * 2}px`;
+ bar.classList.add('bar');
 bar.classList.add('flex-item');
+bars.appendChild(bar);
 }
 }
 
 const newArray = document.querySelector(".newArray");
 newArray.addEventListener("click", function() {
-    createNewArray(arraySize.value)
-    console.log(newArray)
+    console.log("from newarray" + arraySize.value);
+    console.log("form newarray" + delay);
 })
 
-
-/*let array = [];
-array = [];
-for (let i = 0; i < 100; i++) {
-    array.push(Math.floor(Math.random() * 250) + 1);
-}
-console.log(array) */
