4dd67e0b5967b187ba027b6d1cf9f10229ca90d4
nikitaawasarmol <nikitaawasarmol@gmail.com>
2021-06-02 23:43:40 +0530

    modified css and js files

--- js_files/sorting.js
+++ js_files/sorting.js
@@ -17,7 +17,7 @@ const bars = document.querySelector('#bars');
 
 for(let i = 0; i < noOfBars; i++) {
 const bar = document.createElement("div");
- bar.style.height = `${array[i] * 2}px`;
+ bar.style.height = `${array[i] * 2.7}px`;
  bar.classList.add('bar');
 bar.classList.add('flex-item');
 bars.appendChild(bar);
@@ -27,6 +27,6 @@ bars.appendChild(bar);
 const newArray = document.querySelector(".newArray");
 newArray.addEventListener("click", function() {
     console.log("from newarray" + arraySize.value);
-    console.log("form newarray" + delay);
+    // console.log("form newarray" + delay);
 })
 
---
+++ styles/extra.js
@@ -0,0 +1,35 @@
+let arraySize = document.querySelector('#arr_sz');
+
+let array = [];
+
+
+createNewArray();
+
+function createNewArray(noOfBars = 60) {
+  
+    array = [];
+    for (let i = 0; i < noOfBars; i++) {
+        array.push(Math.floor(Math.random() * 250) + 1);
+    }
+    
+    console.log(array);
+
+    const bars = document.querySelector("#bars");
+
+    for (let i = 0; i < noOfBars; i++) {
+        const bar = document.createElement("div");
+        bar.style.height = `${array[i]*2}px`;
+        bar.classList.add('bar');
+        bar.classList.add('flex-item');
+        bars.appendChild(bar);
+    }
+}
+
+
+
+const newArray = document.querySelector(".newArray");
+newArray.addEventListener("click", function(){
+    console.log("From newArray " + arraySize.value);
+    console.log("From newArray " + delay);
+  
+});
