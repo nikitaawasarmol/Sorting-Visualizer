da0e721c5374eb0484fca75e9d958d1fe50b7b56
nikitaawasarmol <nikitaawasarmol@gmail.com>
2021-06-03 22:02:25 +0530

    implemented bubble-sort

---
+++ js_files/bubble.js
@@ -0,0 +1,35 @@
+console.log("First line of code")
+
+async function bubble() {
+    console.log("in bubble function");
+    const element = document.querySelectorAll(".bar");
+    for(let i = 0; i < element.length - 1;i++) {
+        for(let j = 0; j < element.length -i -1; j++) {
+            element[j].style.background = 'red';
+            element[j + 1].style,background = 'red';
+            if(parseInt(element[j].style.height) > parseInt(element[j + 1].style.height)) {
+                await waitforme(delay);
+                swap(element[j], element[j + 1]);
+            }
+            element[j].style.background = '#b7cece';
+            element[j + 1].style.background = '#b7cece';
+        }
+        element[element.length - 1 -i].style.background = 'green';
+    }
+    element[0].style.background = 'green';
+}
+
+const bubbleSorting = document.querySelector(".bubbleSort");
+console.log(bubbleSorting);
+console.log("outsite bubble function")
+bubbleSorting.addEventListener('click', async function() {
+   console.log("After clicking");
+//    disableSortingBtn();
+//    disableSizeSlider();
+//    disableNewArrayBtn();
+      await bubble();
+//    enableSortingBtn();
+//    enableSizeSlider();
+//    enableNewArrayBtn();
+   console.log("end line of the code");
+});
--- js_files/sorting.js
+++ js_files/sorting.js
@@ -1,5 +1,36 @@
+function swap(element1, element2) {
+    console.log('in swap function');
+
+    let temp = element1.style.height;
+    element1.style.height = element2.style.height;
+    element2.style.height = temp;
+
+}
+
+function waitforme(milisec) { 
+    return new Promise(resolve => { 
+        setTimeout(() => { resolve('') }, milisec); 
+    }) 
+}
+
 let arraySize = document.querySelector('#arr_sz');
 
+
+// Event listener to update the bars on the UI
+arraySize.addEventListener('input', function(){
+    console.log(arraySize.value, typeof(arraySize.value));
+    createNewArray(parseInt(arraySize.value));
+});
+
+// Default input for waitforme function (260ms)
+let delay = 260;
+
+let delayElement = document.querySelector('#speed_input');
+// Event listener to update delay time 
+delayElement.addEventListener('input', function(){
+    console.log(delayElement.value, typeof(delayElement.value));
+    delay = 320 - parseInt(delayElement.value);
+});
 let array = [];
 
 
@@ -27,6 +58,6 @@ bars.appendChild(bar);
 const newArray = document.querySelector(".newArray");
 newArray.addEventListener("click", function() {
     console.log("from newarray" + arraySize.value);
-    // console.log("form newarray" + delay);
+    console.log("form newarray" + delay);
 })
 
